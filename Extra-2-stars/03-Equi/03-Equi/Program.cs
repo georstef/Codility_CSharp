e036fdad94a328f103f90927b8548b175f5bc0eb
georstef <georstef@gmail.com>
2013-11-06 20:37:36 +0200

    equi

---
+++ Extra-2-stars/03-Equi/03-Equi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_Equi
+{
+  //codility code starts here
+  class Solution
+  {
+    public int solution(int[] A)
+    {
+      long[] a = new long[A.Length + 1];
+      for (int i = 0; i < A.Length; i++)
+      {
+        a[i + 1] = a[i] + A[i];
+      }
+
+      for (int i = 0; i < A.Length; i++)
+      {
+        long lsum = a[i] - a[0];
+        long rsum = a[A.Length] - a[i + 1];
+        if (lsum == rsum)
+        {
+          return i;
+        }
+      }
+      return -1;
+    }
+  }
+  //codility code ends here
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Solution sol = new Solution();
+      Console.WriteLine(sol.solution(new int[7] { -7, 1, 5, 2, -4, 3, 0 }));//3
+
+      Console.ReadKey();
+    }
+  }
+}
