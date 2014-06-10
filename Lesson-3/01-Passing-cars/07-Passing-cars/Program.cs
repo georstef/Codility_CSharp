c56ae7241b7e3b3fd9b1e53775bfcb0b72309524
georstef <georstef@gmail.com>
2014-06-10 23:25:25 +0300

    3-1

---
+++ Lesson-3/01-Passing-cars/07-Passing-cars/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07_Passing_cars
+{
+  //codility code starts here
+  class Solution
+  {
+    public int solution(int[] A)
+    {
+      int[] a = new int[A.Length];
+      for (int i = 0; i < A.Length; i++)
+      {
+        a[i] = a[Math.Max(0, i - 1)] + A[i];
+      }
+
+      long result = 0;
+      for (int i = 0; i < A.Length; i++)
+        if (A[i] == 0)
+        {
+          result += (a[A.Length-1] - a[i]);
+        }
+
+      if (result > 1000000000)
+      {
+        return -1;
+      }
+      return (int)result;
+    }
+  }
+  //codility code ends here
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Solution sol = new Solution();
+
+      Console.WriteLine(sol.solution(new int[5] { 0, 1, 0, 1, 1})); // 5
+
+      Console.ReadKey();
+    }
+  }
+}
