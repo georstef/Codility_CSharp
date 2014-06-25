dc6b5a5a7f7cd2a45305d596f8053566ec49fa7b
georstef <georstef@gmail.com>
2014-06-25 20:35:18 +0300

    extra 3-2

---
+++ Extra-3-stars/02-Prefix-set/02-Prefix-set/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02_Prefix_set
+{
+  //codility code starts here
+  class Solution
+  {
+    public int solution(int[] A)
+    {
+      int result = 0;
+      int[] a = new int[A.Length];
+      for (int i = 0; i < A.Length; i++)
+      {
+        if (a[A[i]] == 0)
+        {
+          a[A[i]] = 1;
+          result = i;
+        }
+      }
+      return result;
+    }
+  }
+  //codility code ends here
+  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Solution sol = new Solution();
+      Console.WriteLine(sol.solution(new int[5] { 2, 2, 1, 0, 1 }));//3
+
+      Console.ReadKey();
+    }
+  }
+}
